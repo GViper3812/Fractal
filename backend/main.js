cecf356f38c9f3e41a9d0cb80106bb8e90b56483
GViper3812 <omarf.14@hotmail.com>
2025-06-07 17:00:12 +0300

    Backend Register Funcaionality Setup and Reorganization
    The register functionality is now setup and reachable. Reorganized the main.js file to achieve necessary functions through a router instead of being handled completely in one file.

--- backend/main.js
+++ backend/main.js
@@ -3,52 +3,18 @@ const app = express();
 const cors = require('cors');
 const PORT = 3000;
 
-const db = require('./database');
-
 app.use(cors());
 app.use(express.json());
 
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
 
-    if (!username || !password) {
-        return res.status(400).json({
-            success: false,
-            errorType: "missing_fields",
-            message: "Please fill all fields"
-        });
-    }
 
-    const sql = `SELECT * FROM users WHERE username = ? and password = ?`;
-    
-    db.get(sql, [username, password], (err, row) => {
-        if (err) {
-            return res.status(500).json({
-                success: false,
-                errorType: "database_error",
-                message: "Server Error"
-            });
-        }
+const loginRoute = require('./function/login');
+const registerRoute = require('./function/register');
 
-        if (!row) {
-            return res.status(404).json({
-                success: false,
-                errorType: "invalid_credentials",
-                message: "Invalid Credentials"
-            });
-        }
+app.use('/', loginRoute);
+app.use('/', registerRoute);
 
-        res.status(200).json({
-            success: true,
-            errorType: null,
-            message: "Login Confirmed"
-        });
-    });
-});
 
-app.post('/test/:id', (req, res) => {
-
-});
 
 app.listen(
     PORT, 
